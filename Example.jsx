351e11f5b6817a942f610e9315650bfaf0a3e5d1
ReedKrawiec <reedkrawiec@gmail.com>
2016-11-23 17:32:39 -0500

    Fixed alt tabbing error

--- Example.jsx
+++
@@ -1,7 +0,0 @@
-import TyperWrapper from "./typer.jsx";
-import * as React from "react";
-import {render} from "react-dom";
-render(
-  <div>
-   <TyperWrapper words={["Hello!","Bonjour!","你好!","Kamusta!","Cześć!","こんにちは!","नमस्ते!"]} delay={5000} />
-  </div>,document.querySelector(".mount"))
